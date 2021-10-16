9e461ca6bdd94f6b7ecfba7dd5937bef542b834b
Maximilian Kaske <maximilian@kaske.org>
2021-10-16 22:42:54 +0200

    chore: add mx

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const gallery = document.getElementById("gallery");
+const wrapper = document.getElementById("gallery-wrapper");
+console.log(wrapper.childNodes);
 const tag = document.getElementById("placeholder");
-const element = document.getElementById("item-0");
-const cardWidth = element.offsetWidth;
+const element = document.getElementById("t");
+const cardWidth = wrapper.childNodes[1].offsetWidth;
+
 let lastIndex = 0;
 let activeIndex = 0;
+
+// TODO: inject onclick to `item-${number}` elements
 
 function getPlaceholderWidth() {
   return (
@@ -19,10 +24,10 @@ function setPlaceholderWidth() {
 }
 
 function getScrollX() {
+  console.log(cardWidth, gallery.scrollLeft);
   if (gallery.scrollLeft % cardWidth === 0) {
     lastIndex = activeIndex;
     activeIndex = gallery.scrollLeft / cardWidth;
-    console.log(lastIndex, activeIndex);
     const activeItem = document.getElementById(`item-${activeIndex}`);
     const lastItem = document.getElementById(`item-${lastIndex}`);
     lastItem.classList.remove("border-2", "border-indigo-600");
